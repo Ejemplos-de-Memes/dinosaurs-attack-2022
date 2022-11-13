9bc7ddaefdf8f33e9956b9eead5cc627ce121664
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-11-13 16:12:57 -0300

    debug: arreglar los nombres del main

--- src/main/java/cl/uchile/dcc/Main.java
+++ src/main/java/cl/uchile/dcc/Main.java
@@ -6,10 +6,6 @@ import cl.uchile.dcc.model.items.bag.Bag;
 import cl.uchile.dcc.model.items.food.Herb;
 import cl.uchile.dcc.model.items.food.Meat;
 import cl.uchile.dcc.model.items.food.NullFood;
-import cl.uchile.dcc.model.states.FuriousMood;
-import cl.uchile.dcc.model.states.NormalMood;
-import cl.uchile.dcc.model.states.SadMood;
-import cl.uchile.dcc.model.states.TiredMood;
 
 public class Main {
 
@@ -29,16 +25,16 @@ public class Main {
     System.out.println(tRex);
     System.out.println(otherTRex);
 
-    Bag bag1 = new Bag("Principal");
-    Bag bag2 = new Bag("Mochila 1");
-    bag2.add(new Herb(42));
-    bag1.add(bag2);
-    bag2.add(new Meat(32, 23));
-    Bag bag3 = new Bag("Mochila 2");
-    bag1.add(bag3);
-    bag3.add(new Meat(13, 31));
-    bag1.add(new NullFood());
-    System.out.println(bag1);
+    Bag principal = new Bag("Principal");
+    Bag mochila1 = new Bag("Mochila 1");
+    mochila1.add(new Herb(42));
+    principal.add(mochila1);
+    mochila1.add(new Meat(32, 23));
+    Bag mochila2 = new Bag("Mochila 2");
+    principal.add(mochila2);
+    mochila2.add(new Meat(13, 31));
+    principal.add(new NullFood());
+    System.out.println(principal);
 
   }
 }
